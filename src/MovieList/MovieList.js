7e2c4444a108cb48debc7fb2b55e78b2e81dd68a
networkdavit <lonecreation1612@gmail.com>
2023-02-12 01:01:13 +0400

    first commit

---
+++ src/MovieList/MovieList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./MovieList.css";
+
+function MoviesList() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:8000/api/v1/movies/")
+      .then(res => res.json())
+      .then(data => {
+        setMovies(data.results);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="movies-container">
+      {error && <p>Something went wrong: {error.message}</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className="movies-list">
+          {movies.map(movie => (
+            <div className="movie-box" key={movie.id}>
+              <h3>Title</h3>
+              <p>{movie.title}</p>
+              <h5>Release year</h5>
+              <p>{movie.year}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MoviesList;
